dabc22f2db3e4283f93c623d94bf54839779117a
adam2o2 <78441732+adam2o2@users.noreply.github.com>
2024-06-07 17:34:13 -0500

    words move on scroll

---
+++ script.js
@@ -0,0 +1,22 @@
+//this is ment for the words moving to the right
+document.addEventListener("DOMContentLoaded", function() {
+    const recommendTexts = document.querySelectorAll('.recommend-text, .recommend-text-1, .recommend-text-2');
+
+    function checkPosition() {
+        const triggerBottom = window.innerHeight / 5 * 4;
+
+        recommendTexts.forEach(text => {
+            const textTop = text.getBoundingClientRect().top;
+
+            if (textTop < triggerBottom) {
+                text.classList.add('in-view');
+            } else {
+                text.classList.remove('in-view');
+            }
+        });
+    }
+
+    window.addEventListener('scroll', checkPosition);
+    checkPosition();
+});
+// this is ment for the words moving to the right
