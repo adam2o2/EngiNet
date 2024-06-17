b0339c625c1902b7fb9d55235d789c2272fbadb6
adam2o2 <78441732+adam2o2@users.noreply.github.com>
2024-06-16 22:56:41 -0500

    added about

--- script.js
+++ script.js
@@ -138,3 +138,23 @@ function goBack(detailsId) {
 }
 
 // creates the anime trailer click to the desire location
+
+document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.getElementById("aboutModal");
+    var btn = document.getElementById("aboutButton");
+    var span = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
